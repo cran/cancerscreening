55f88777fd32f824b1e0a5c2ea2d44c3d3a5e781
David Kariuki <hello@damurka.com>
2024-04-14 12:30:02 +0000

    version 1.1.0

--- inst/doc/cancerscreening.R
+++ inst/doc/cancerscreening.R
@@ -1,14 +1,14 @@
 ## ----setup, include = FALSE---------------------------------------------------
 auth_success <- tryCatch(
-  cancerscreening:::khis_cred_docs(),
-  cancerscreening_cred_internal_error = function(e) e
+  khisr:::khis_cred_docs(),
+  khis_cred_internal_error = function(e) e
 )
 
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
   error = TRUE,
-  purl = cancerscreening::khis_has_cred(),
-  eval = cancerscreening::khis_has_cred()
+  purl = khisr::khis_has_cred(),
+  eval = khisr::khis_has_cred()
 )
 
